9153be198eba3b2c1be36bba5d976be1584a98a2
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2024-10-24 21:39:08 +0300

    wrap app with GestureHandlerRootView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
 import { Stack } from "expo-router";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <GestureHandlerRootView style={styles.container}>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
