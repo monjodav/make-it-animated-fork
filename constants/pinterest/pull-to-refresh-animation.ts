d210c5bc8e2e8823b1ddbb12ff78b516a6d7d22e
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2024-12-11 11:18:10 +0200

    add telegram stories

--- constants/pinterest/pull-to-refresh-animation.ts
+++ constants/pinterest/pull-to-refresh-animation.ts
@@ -4,7 +4,7 @@ const loadingIndicatorDiameter = 44;
 
 export const sharedConfigs = {
   loadingIndicatorDiameter,
-  refreshingTriggerOffset: loadingIndicatorDiameter,
+  refreshingTriggerOffset: loadingIndicatorDiameter * 1.5,
   wrapperHeightOnRefreshing: 150,
   wrapperHeightOnRefreshingAnimDuration: 350,
   onRefreshCompleteDuration: 500,
