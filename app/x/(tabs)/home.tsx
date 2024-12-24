4f67347243d841cb5a0d1e07e311d1c8ee35d88c
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2024-12-24 12:40:45 +0200

    add useHeaderAnimation hook

--- app/x/(tabs)/home.tsx
+++ app/x/(tabs)/home.tsx
@@ -1,189 +1,48 @@
 import { HomeHeader } from "@/components/x/home-header";
-import { HomePost } from "@/components/x/home-post";
+import { HomeList } from "@/components/x/home-list";
+import { useHeaderAnimation } from "@/hooks/x/use-header-animation";
 import { XTabsContext } from "@/providers/x-tabs-provider";
 import { BlurView } from "expo-blur";
 import { useContext, useState } from "react";
-import { StyleSheet, View } from "react-native";
-import Animated, {
-  Extrapolation,
-  interpolate,
-  useAnimatedReaction,
-  useAnimatedScrollHandler,
-  useAnimatedStyle,
-  useDerivedValue,
-  useSharedValue,
-  withTiming,
-} from "react-native-reanimated";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
+import Animated from "react-native-reanimated";
 
 // x-bottom-tabs-background-animation 🔽
-// x-home-header-animation 🔽
 
-const _onEndDragAnimDuration = 100;
+type PostList = {
+  title: string;
+};
+
+const posts: PostList[] = [
+  {
+    title: "All",
+  },
+  {
+    title: "ColorsApp",
+  },
+  {
+    title: "project 1",
+  },
+];
 
 export default function Home() {
   const [headerHeight, setHeaderHeight] = useState(0);
 
   const { tabBarHeight, scrollDirection, handleXTabsOnScroll } = useContext(XTabsContext);
 
-  const listOffsetY = useSharedValue(0);
-  const listOffsetYRefPoint = useSharedValue(0);
-  const isListDragging = useSharedValue(false);
+  const { width } = useWindowDimensions();
 
-  const headerOpacity = useSharedValue(1);
-  const headerOpacityRefPoint = useSharedValue(1);
-  const headerTranslateY = useSharedValue(0);
-  const headerTranslateYRefPoint = useSharedValue(0);
-
-  const headerState = useDerivedValue(() => {
-    if (headerTranslateY.value === 0) {
-      return "visible";
-    }
-
-    if (headerTranslateY.value === -headerHeight) {
-      return "hidden";
-    }
+  // x-home-header-animation 🔽
+  const { rHeaderStyle, rBlurViewStyle, scrollHandler } = useHeaderAnimation({
+    headerHeight,
+    scrollDirection,
+    handleXTabsOnScroll,
   });
-  const headerTransition = useSharedValue(false);
-
-  useAnimatedReaction(
-    () => {
-      return scrollDirection.value;
-    },
-    (currentValue, previousValue) => {
-      if (currentValue !== previousValue) {
-        listOffsetYRefPoint.value = listOffsetY.value;
-        headerOpacityRefPoint.value = headerOpacity.value;
-        headerTranslateYRefPoint.value = headerTranslateY.value;
-      }
-    }
-  );
-
-  const scrollHandler = useAnimatedScrollHandler({
-    onMomentumBegin: () => {
-      isListDragging.value = true;
-    },
-    onBeginDrag: (e) => {
-      isListDragging.value = true;
-      listOffsetYRefPoint.value = e.contentOffset.y;
-      headerOpacityRefPoint.value = headerOpacity.value;
-      headerTranslateYRefPoint.value = headerTranslateY.value;
-    },
-    onScroll: (e) => {
-      listOffsetY.value = e.contentOffset.y;
-      handleXTabsOnScroll(e);
-    },
-    onEndDrag: () => {
-      isListDragging.value = false;
-
-      if (listOffsetY.value < headerHeight) {
-        headerOpacity.value = withTiming(1, { duration: _onEndDragAnimDuration * 2 });
-        headerTranslateY.value = withTiming(0, { duration: _onEndDragAnimDuration * 2 });
-        headerTransition.value = false;
-        return;
-      }
-
-      if (
-        listOffsetY.value > headerHeight &&
-        headerTransition.value === true &&
-        scrollDirection.value === "down"
-      ) {
-        headerOpacity.value = withTiming(0, { duration: _onEndDragAnimDuration });
-        headerTranslateY.value = withTiming(-headerHeight, { duration: _onEndDragAnimDuration });
-        headerTransition.value = false;
-        return;
-      }
-
-      if (
-        listOffsetY.value > headerHeight &&
-        headerTransition.value === true &&
-        scrollDirection.value === "up"
-      ) {
-        headerOpacity.value = withTiming(1, { duration: _onEndDragAnimDuration });
-        headerTranslateY.value = withTiming(0, { duration: _onEndDragAnimDuration });
-        headerTransition.value = false;
-        return;
-      }
-    },
-    // onMomentumEnd: () => {
-    //   isListDragging.value = false;
-    // },
-  });
-
-  const rHeaderStyle = useAnimatedStyle(() => {
-    if (
-      listOffsetY.value > 0 &&
-      scrollDirection.value === "down" &&
-      isListDragging.value === true
-    ) {
-      if (headerState.value === "hidden") {
-        return {
-          opacity: 0,
-          transform: [{ translateY: -headerHeight }],
-        };
-      }
-      headerTransition.value = true;
-      headerOpacity.value = interpolate(
-        listOffsetY.value,
-        [listOffsetYRefPoint.value, listOffsetYRefPoint.value + headerHeight / 2],
-        [headerOpacityRefPoint.value, 0],
-        Extrapolation.CLAMP
-      );
-      headerTranslateY.value = interpolate(
-        listOffsetY.value,
-        [listOffsetYRefPoint.value, listOffsetYRefPoint.value + headerHeight],
-        [headerTranslateYRefPoint.value, -headerHeight],
-        Extrapolation.CLAMP
-      );
-    }
-
-    if (listOffsetY.value > 0 && scrollDirection.value === "up" && isListDragging.value === true) {
-      if (headerState.value === "visible") {
-        return {
-          opacity: 1,
-          transform: [{ translateY: 0 }],
-        };
-      }
-      headerTransition.value = true;
-      headerOpacity.value = interpolate(
-        listOffsetY.value,
-        [listOffsetYRefPoint.value, listOffsetYRefPoint.value - 2 * headerHeight],
-        [headerOpacityRefPoint.value, 1],
-        Extrapolation.CLAMP
-      );
-      headerTranslateY.value = interpolate(
-        listOffsetY.value,
-        [listOffsetYRefPoint.value, listOffsetYRefPoint.value - 2 * headerHeight],
-        [headerTranslateYRefPoint.value, 0],
-        Extrapolation.CLAMP
-      );
-    }
-
-    return {
-      opacity: headerOpacity.value,
-      transform: [
-        {
-          translateY: headerTranslateY.value,
-        },
-      ],
-    };
-  });
-
-  const rBlurViewStyle = useAnimatedStyle(() => {
-    return {
-      opacity: withTiming(listOffsetY.value > headerHeight ? 1 : 0),
-    };
-  });
-
-  const _renderItem = () => {
-    return <HomePost />;
-  };
-
-  const _renderItemSeparator = () => {
-    return <View className="h-px bg-x-front my-6" />;
-  };
+  // x-home-header-animation 🔼
 
   return (
     <View className="flex-1 bg-x-back">
+      {/* x-home-header-animation 🔽 */}
       <Animated.View
         style={rHeaderStyle}
         className="absolute top-0 left-0 right-0 z-50"
@@ -198,22 +57,25 @@ export default function Home() {
         </Animated.View>
         <HomeHeader />
       </Animated.View>
+      {/* x-home-header-animation 🔼 */}
       <Animated.FlatList
-        data={Array.from({ length: 20 })}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={_renderItem}
-        ItemSeparatorComponent={_renderItemSeparator}
-        contentContainerClassName="pt-5"
-        contentContainerStyle={{
-          paddingTop: headerHeight + 16,
-          paddingBottom: tabBarHeight + 16,
-        }}
-        scrollEventThrottle={1000 / 60}
-        onScroll={scrollHandler}
+        data={posts}
+        keyExtractor={(item) => item.title}
+        renderItem={() => (
+          <View style={{ width }}>
+            <HomeList
+              headerHeight={headerHeight}
+              tabBarHeight={tabBarHeight}
+              scrollHandler={scrollHandler}
+            />
+          </View>
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        pagingEnabled
       />
     </View>
   );
 }
 
-// x-home-header-animation 🔼
 // x-bottom-tabs-background-animation 🔼
---
+++ components/x/home-list.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from "react";
+import { View } from "react-native";
+import Animated, { ScrollHandlerProcessed } from "react-native-reanimated";
+import { HomePost } from "./home-post";
+
+type Props = {
+  headerHeight: number;
+  tabBarHeight: number;
+  scrollHandler: ScrollHandlerProcessed<Record<string, unknown>>;
+};
+
+export const HomeList: FC<Props> = ({ headerHeight, tabBarHeight, scrollHandler }) => {
+  const _renderItem = () => {
+    return <HomePost />;
+  };
+
+  const _renderItemSeparator = () => {
+    return <View className="h-px bg-x-front my-6" />;
+  };
+
+  return (
+    <Animated.FlatList
+      data={Array.from({ length: 20 })}
+      keyExtractor={(_, index) => index.toString()}
+      renderItem={_renderItem}
+      ItemSeparatorComponent={_renderItemSeparator}
+      contentContainerClassName="pt-5"
+      contentContainerStyle={{
+        paddingTop: headerHeight + 16,
+        paddingBottom: tabBarHeight + 16,
+      }}
+      scrollEventThrottle={1000 / 60}
+      onScroll={scrollHandler}
+    />
+  );
+};
