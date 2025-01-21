251339bde558f07f25ae088b771f71306a377ea6
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-01-21 22:22:41 +0200

    add viber-top-tabs-indicator-animation

--- app/viber/calls.tsx
+++ app/viber/calls.tsx
@@ -1,6 +1,6 @@
 import { ScreenHeader } from "@/components/viber/screen-header";
 import { SearchBar } from "@/components/viber/search-bar";
-import React from "react";
+import React, { useState } from "react";
 import { View, Alert } from "react-native";
 import Animated, {
   Extrapolation,
@@ -13,10 +13,18 @@ import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { CallsListItem } from "@/components/viber/calls-list-item";
 import { Title } from "@/components/viber/title";
+import { TopTabs } from "@/components/viber/top-tabs";
+
+export enum Tab {
+  Contacts = 0,
+  Recent = 1,
+}
 
 const _titleHeight = 36;
 
 export default function Calls() {
+  const [activeTab, setActiveTab] = useState(Tab.Contacts);
+
   const listOffsetY = useSharedValue(0);
   const isHeaderTitleVisible = useSharedValue(false);
 
@@ -45,17 +53,6 @@ export default function Calls() {
     };
   });
 
-  const rTitleStyle = useAnimatedStyle(() => {
-    return {
-      height: interpolate(
-        listOffsetY.value,
-        [0, _titleHeight],
-        [_titleHeight, 0],
-        Extrapolation.CLAMP
-      ),
-    };
-  });
-
   return (
     <View className="flex-1 bg-black">
       <ScreenHeader
@@ -66,25 +63,44 @@ export default function Calls() {
         onRightButton2Press={() => Alert.alert("Open keypad")}
         isHeaderTitleVisible={isHeaderTitleVisible}
       />
-
       <Animated.View className="px-5 pb-4">
         <View className="mb-4">
           <Title listOffsetY={listOffsetY} inputRange={[0, _titleHeight]} />
         </View>
-        <SearchBar />
+        <View className="mb-3">
+          <SearchBar />
+        </View>
+        {/* viber-top-tabs-indicator-animation 🔽 */}
+        <TopTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        {/* viber-top-tabs-indicator-animation 🔼 */}
         <View className="h-px bg-neutral-800/75 -mx-5 mt-6 mb-2" />
-        <View className="h-[75px] bg-neutral-900 rounded-2xl" />
+        {activeTab === Tab.Contacts && <View className="h-[75px] bg-neutral-900 rounded-2xl" />}
       </Animated.View>
-      <Animated.FlatList
-        data={Array.from({ length: 20 }, (_, index) => index)}
-        renderItem={({ item }) => <CallsListItem key={item} />}
-        ListHeaderComponent={() => <View className="h-[75px] bg-neutral-900 rounded-2xl" />}
-        contentContainerClassName="gap-4 px-5"
-        style={rListStyle}
-        indicatorStyle="white"
-        scrollEventThrottle={1000 / 60}
-        onScroll={scrollHandler}
-      />
+      {activeTab === Tab.Contacts && (
+        <Animated.FlatList
+          data={Array.from({ length: 20 }, (_, index) => index)}
+          renderItem={({ item }) => <CallsListItem key={item} />}
+          ListHeaderComponent={() => <View className="h-[75px] bg-neutral-900 rounded-2xl" />}
+          contentContainerClassName="gap-4 px-5"
+          style={rListStyle}
+          indicatorStyle="white"
+          scrollEventThrottle={1000 / 60}
+          onScroll={scrollHandler}
+        />
+      )}
+      {activeTab === Tab.Recent && (
+        <Animated.FlatList
+          data={[]}
+          renderItem={({ item }) => <CallsListItem key={item} />}
+          ListEmptyComponent={() => (
+            <View className="mt-[100px] items-center justify-center gap-6">
+              <View className="w-[120px] h-[150px] bg-neutral-900 rounded-3xl" />
+              <View className="w-[100px] h-8 bg-neutral-900 rounded-full" />
+            </View>
+          )}
+          contentContainerClassName="gap-4 px-5"
+        />
+      )}
     </View>
   );
 }
---
+++ components/viber/top-tabs.tsx
@@ -0,0 +1,81 @@
+import { Tab } from "@/app/viber/calls";
+import React, { FC } from "react";
+import { View, Pressable } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+
+// viber-top-tabs-indicator-animation 🔽
+
+const _padding = 3;
+
+type TabItemProps = {
+  title: string;
+  active: boolean;
+  onPress: () => void;
+};
+
+const TabItem: FC<TabItemProps> = ({ title, active, onPress }) => {
+  const rTextStyle = useAnimatedStyle(() => {
+    return {
+      color: withTiming(active ? "#FFFFFF" : "#A1A1A4"),
+      transform: [
+        {
+          scale: withTiming(active ? 1.05 : 0.95),
+        },
+      ],
+    };
+  });
+
+  return (
+    <Pressable className="flex-1 items-center py-[6px]" onPress={onPress}>
+      <Animated.Text className="text-sm font-medium" style={rTextStyle}>
+        {title}
+      </Animated.Text>
+    </Pressable>
+  );
+};
+
+type Props = {
+  activeTab: Tab;
+  setActiveTab: (tab: Tab) => void;
+};
+
+export const TopTabs: FC<Props> = ({ activeTab, setActiveTab }) => {
+  const tabsWidth = useSharedValue(0);
+
+  const rIndicatorStyle = useAnimatedStyle(() => {
+    return {
+      width: tabsWidth.value / 2,
+      left: withTiming(
+        activeTab === Tab.Contacts ? _padding : (tabsWidth.value - 2 * _padding) / 2,
+        {
+          duration: 200,
+        }
+      ),
+    };
+  });
+
+  return (
+    <View
+      className="rounded-lg bg-neutral-900 flex-row items-center"
+      style={{ padding: _padding }}
+      onLayout={(e) => (tabsWidth.value = e.nativeEvent.layout.width)}
+    >
+      <Animated.View
+        className="absolute rounded-[6px] bg-[#7F61F2]"
+        style={[rIndicatorStyle, { top: _padding, bottom: _padding }]}
+      />
+      <TabItem
+        title="Contacts"
+        active={activeTab === Tab.Contacts}
+        onPress={() => setActiveTab(Tab.Contacts)}
+      />
+      <TabItem
+        title="Recent"
+        active={activeTab === Tab.Recent}
+        onPress={() => setActiveTab(Tab.Recent)}
+      />
+    </View>
+  );
+};
+
+// viber-top-tabs-indicator-animation 🔼
