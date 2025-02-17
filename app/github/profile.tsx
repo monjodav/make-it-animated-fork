243451dff2126e24fc312f53faa5bc8f200a11a1
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-02-17 20:34:34 +0200

    add wireframes

--- app/github/profile.tsx
+++ app/github/profile.tsx
@@ -22,7 +22,7 @@ export default function Profile() {
       scrollEventThrottle={1000 / 60}
       indicatorStyle="white"
     >
-      <View className="flex-row items-center gap-4">
+      <View className="flex-row items-center gap-4 mb-6">
         <View className="w-20 h-20 rounded-full bg-neutral-900" />
         <View className="flex-1">
           <Text className="text-stone-100 font-bold text-lg">Volodymyr Serbulenko</Text>
@@ -39,6 +39,27 @@ export default function Profile() {
           </Animated.Text>
         </View>
       </View>
+
+      <View className="w-full h-12 bg-neutral-950 rounded-xl mb-6" />
+
+      <View className="w-3/4 h-4 bg-neutral-950 rounded-md mb-1" />
+      <View className="w-1/2 h-4 bg-neutral-950 rounded-md mb-4" />
+
+      <View className="w-32 h-4 bg-neutral-950 rounded-md mb-4" />
+
+      <View className="flex-row items-center gap-1 mb-6">
+        <View className="w-4 h-4 rounded-full bg-neutral-950" />
+        <View className="w-16 h-4 bg-neutral-950 rounded-md" />
+        <View className="w-4 h-4 rounded-full bg-neutral-950" />
+        <View className="w-20 h-4 bg-neutral-950 rounded-md" />
+      </View>
+      <View className="gap-[2px] mb-10">
+        <View className="w-full h-12 bg-neutral-950 rounded-lg" />
+        <View className="w-full h-12 bg-neutral-950 rounded-lg" />
+        <View className="w-full h-12 bg-neutral-950 rounded-lg" />
+      </View>
+      <View className="w-1/3 h-4 bg-neutral-950 rounded-md mb-4" />
+      <View className="w-full h-40 bg-neutral-950 rounded-xl mb-6" />
     </Animated.ScrollView>
   );
 }
