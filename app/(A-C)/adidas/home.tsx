ccb4d03c1a22b64f63810c3fd596975e2603938f
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-03-08 18:31:22 +0200

    transfer apple invites

--- app/(A-C)/adidas/home.tsx
+++ app/(A-C)/adidas/home.tsx
@@ -1,2 +1,3 @@
 import Home from "@/src/apps/A-C/adidas/routes/home";
+
 export default Home;
--- app/(A-C)/apple-books/book.tsx
+++ app/(A-C)/apple-books/book.tsx
@@ -1,27 +1,3 @@
-import { Menu } from "@/components/A-C/apple-books/menu";
-import { View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Book from "@/src/apps/A-C/apple-books/routes/book";
 
-// apple-books-menu-buttons-animation 🔽
-
-export default function Book() {
-  const insets = useSafeAreaInsets();
-
-  return (
-    <View
-      className="flex-1 bg-black"
-      style={{ paddingTop: insets.top + 16, paddingBottom: insets.bottom }}
-    >
-      <View className="flex-1 items-center px-5">
-        <View className="w-[60px] h-5 rounded-full bg-neutral-800 mb-10" />
-        <View className="w-[200px] h-8 rounded-full bg-neutral-800 mb-1" />
-        <View className="w-[180px] h-8 rounded-full bg-neutral-800 mb-[50px]" />
-        <View className="w-[80px] h-1 rounded-full bg-neutral-800 mb-[50px]" />
-        <View className="w-[90px] h-6 rounded-full bg-neutral-800 mb-10" />
-      </View>
-      <Menu />
-    </View>
-  );
-}
-
-// apple-books-menu-buttons-animation 🔼
+export default Book;
