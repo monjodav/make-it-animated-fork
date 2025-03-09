9f89f6adb5c94c86c19948f165d75e829b35d8c3
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-03-09 11:51:56 +0200

    transfer luma

---
+++ src/apps/(j-l)/luma/routes/discover-city.tsx
@@ -0,0 +1,57 @@
+import { Alert, Platform, View } from "react-native";
+
+import Animated, { useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useEffect } from "react";
+import { EventItem } from "../components/event-item";
+import { Header } from "../components/header";
+import { HeaderImage } from "../components/header-image";
+
+// luma-blurred-header-image-animation 🔽
+
+export default function DiscoverCity() {
+  const insets = useSafeAreaInsets();
+
+  const scrollY = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: ({ contentOffset: { y } }) => {
+      scrollY.value = y;
+    },
+  });
+
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      Alert.alert("Note", "Bounce effect is not supported on Android. Please try it on iOS.");
+    }
+  }, []);
+
+  const _renderHeader = () => <Header />;
+
+  const _renderItem = () => {
+    return (
+      <View className="px-4 py-2 bg-black">
+        <EventItem />
+      </View>
+    );
+  };
+
+  return (
+    <View className="flex-1 bg-black">
+      <HeaderImage scrollY={scrollY} />
+      <Animated.FlatList
+        data={Array.from({ length: 8 }, (_, index) => index)}
+        ListHeaderComponent={_renderHeader}
+        renderItem={_renderItem}
+        contentContainerStyle={{
+          paddingBottom: insets.bottom,
+        }}
+        showsVerticalScrollIndicator={false}
+        onScroll={scrollHandler}
+        scrollEventThrottle={1000 / 60}
+      />
+    </View>
+  );
+}
+
+// luma-blurred-header-image-animation 🔼
