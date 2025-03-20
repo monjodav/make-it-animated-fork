6e5bcda5d0fa8a4c4cd3077f20673c9240e3369a
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-03-20 18:31:00 +0200

    upd APP_STORE_URL

--- src/shared/lib/constants/links.ts
+++ src/shared/lib/constants/links.ts
@@ -1,4 +1,4 @@
 export const WEBSITE_URL = "https://makeitanimated.dev";
 // TODO: Add Play Market URL
 export const PLAY_MARKET_URL = "";
-export const APP_STORE_URL = "";
+export const APP_STORE_URL = "https://apps.apple.com/us/app/make-it-animated/id6742489722";
