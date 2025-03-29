9a778e1de9ea51dc76c629425aa8be6564920151
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-03-29 19:10:04 +0200

    bump up version to 1.1.1

--- app.config.ts
+++ app.config.ts
@@ -36,7 +36,7 @@ export default ({ config }: { config: ExpoConfig }) => ({
   expo: {
     name: getEnvironmentValues().name,
     slug: "make-it-animated",
-    version: "1.1.0",
+    version: "1.1.1",
     orientation: "portrait",
     scheme: "miaapp",
     userInterfaceStyle: "automatic",
