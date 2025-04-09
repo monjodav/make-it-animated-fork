b4ff322ffa7f488524f5680ca4e70c820c9a7777
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-04-09 17:12:55 +0300

    fix whatsapp large title set issue

--- src/apps/(u-z)/whatsapp/components/communities-content.tsx
+++ src/apps/(u-z)/whatsapp/components/communities-content.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Platform, View } from "react-native";
-import { SharedValue } from "react-native-reanimated";
+import Animated, { SharedValue } from "react-native-reanimated";
 import { useHeaderBackground } from "../lib/hooks/use-header-background";
 
 type Props = {
@@ -8,13 +8,10 @@ type Props = {
 };
 
 export const CommunitiesContent: FC<Props> = ({ offsetY }) => {
-  const { contentOffsetY } = useHeaderBackground({ offsetY });
+  const { targetRef, onTargetLayout } = useHeaderBackground({ offsetY });
 
   return (
-    <View
-      className="opacity-75 px-4"
-      onLayout={({ nativeEvent }) => contentOffsetY.set(nativeEvent.layout.y)}
-    >
+    <Animated.View ref={targetRef} onLayout={onTargetLayout} className="opacity-75 px-4">
       <View className="h-32 w-32 bg-stone-900 rounded-2xl self-center mb-6" />
 
       <View className="h-6 w-[90%] bg-neutral-900 rounded-full mb-4" />
@@ -27,6 +24,6 @@ export const CommunitiesContent: FC<Props> = ({ offsetY }) => {
       <View className="h-12 bg-green-500/10 rounded-xl" />
 
       {Platform.OS === "android" && <View className="h-[500px] w-full" />}
-    </View>
+    </Animated.View>
   );
 };
