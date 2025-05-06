bf5f2af097ab0bcf5f054c29818380c54c506212
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-05-06 12:09:38 +0300

    add variable blur view for raycast home screen

--- src/apps/(p-r)/raycast/components/home/top-gradient.tsx
+++ src/apps/(p-r)/raycast/components/home/top-gradient.tsx
@@ -1,20 +1,39 @@
 import { LinearGradient } from "expo-linear-gradient";
 import React, { FC } from "react";
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import { colorKit } from "reanimated-color-picker";
 import { useHeaderHeight } from "../../lib/hooks/use-header-height";
+import { BlurView } from "expo-blur";
+import MaskedView from "@react-native-masked-view/masked-view";
 
 // raycast-home-search-transition-animation 🔽
 
 export const TopGradient: FC = () => {
   const { grossHeight } = useHeaderHeight();
 
+  if (Platform.OS === "android") {
+    return (
+      <LinearGradient
+        style={[StyleSheet.absoluteFillObject, { height: grossHeight * 1.2 }]}
+        colors={[colorKit.setAlpha("#171717", 0.9).hex(), colorKit.setAlpha("#171717", 0).hex()]}
+        locations={[0.75, 1]}
+      />
+    );
+  }
+
   return (
-    <LinearGradient
-      style={[StyleSheet.absoluteFillObject, { height: grossHeight }]}
-      colors={[colorKit.setAlpha("#171717", 0.9).hex(), colorKit.setAlpha("#171717", 0).hex()]}
-      locations={[0.75, 1]}
-    />
+    <MaskedView
+      maskElement={
+        <LinearGradient
+          locations={[0.75, 1]}
+          colors={["black", "transparent"]}
+          style={StyleSheet.absoluteFill}
+        />
+      }
+      style={[StyleSheet.absoluteFill, { height: grossHeight * 1.2 }]}
+    >
+      <BlurView tint="dark" style={StyleSheet.absoluteFill} />
+    </MaskedView>
   );
 };
 
