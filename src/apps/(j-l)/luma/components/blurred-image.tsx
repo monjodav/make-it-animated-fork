73e6a61b374ca62b5ad0dad70ad7dda3ba1b787f
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-05-13 21:26:40 +0300

    fix luma app issue

--- src/apps/(j-l)/luma/components/blurred-image.tsx
+++ src/apps/(j-l)/luma/components/blurred-image.tsx
@@ -1,29 +1,11 @@
-import { View, useWindowDimensions, Image, StyleSheet } from "react-native";
+import { View, useWindowDimensions, StyleSheet, Platform } from "react-native";
 import MaskedView from "@react-native-masked-view/masked-view";
 import { LinearGradient } from "expo-linear-gradient";
 import { FC } from "react";
-import { Canvas, Blur, Image as SkImage, useImage } from "@shopify/react-native-skia";
+import { Image } from "expo-image";
+import { BlurView } from "expo-blur";
 
 // luma-blurred-header-image-animation 🔽
-
-type SkiaBlurImageProps = {
-  imageSource: number;
-};
-
-const SkiaBlurImage: FC<SkiaBlurImageProps> = ({ imageSource }) => {
-  const { width, height } = useWindowDimensions();
-  const image = useImage(imageSource);
-  if (!image) {
-    return null;
-  }
-  return (
-    <Canvas style={{ width, height }}>
-      <SkImage x={0} y={0} width={width} height={height} image={image} fit="cover">
-        <Blur blur={100} mode="clamp" />
-      </SkImage>
-    </Canvas>
-  );
-};
 
 type Props = {
   imageSource: number;
@@ -39,15 +21,19 @@ export const BlurredImage: FC<Props> = ({ imageSource }) => {
         <MaskedView
           maskElement={
             <LinearGradient
-              locations={[0, 0.3, 0.5, 1]}
+              locations={[0, 0.4, 0.55, 1]}
               colors={["transparent", "transparent", "black", "black"]}
               style={StyleSheet.absoluteFill}
             />
           }
           style={StyleSheet.absoluteFill}
         >
-          <SkiaBlurImage imageSource={imageSource} />
-          <View className="absolute w-full h-full bg-black/20" />
+          <BlurView
+            intensity={100}
+            tint={Platform.OS === "ios" ? "systemThinMaterial" : "systemUltraThinMaterialDark"}
+            style={{ width, height }}
+            experimentalBlurMethod="dimezisBlurView"
+          />
         </MaskedView>
       </View>
     </View>
