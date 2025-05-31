7041b82b21d8e3296d71fad586046c063bc20d30
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-05-31 21:07:13 +0300

    optimize channels

--- src/apps/(s-t)/slack/components/unread-channels.tsx
+++ src/apps/(s-t)/slack/components/unread-channels.tsx
@@ -1,12 +1,15 @@
-import React, { FC } from "react";
+import React, { FC, memo, useMemo } from "react";
 import { View } from "react-native";
 import { useUnreadStore } from "../lib/store/unread";
 import { ChannelAnimationProvider } from "../lib/provider/channel-animation";
 import { Channel } from "./channel";
+import { Channel as ChannelType } from "../lib/types";
 
-export const UnreadChannels: FC = () => {
-  const unreadChannels = useUnreadStore.use.unreadChannels();
+type Props = {
+  unreadChannels: ChannelType[];
+};
 
+const MemoizedListOfChannels: FC<Props> = memo(({ unreadChannels }) => {
   return (
     <View className="flex-1">
       {unreadChannels.map((channel, index) => (
@@ -16,4 +19,16 @@ export const UnreadChannels: FC = () => {
       ))}
     </View>
   );
+});
+
+MemoizedListOfChannels.displayName = "MemoizedListOfChannels";
+
+export const UnreadChannels: FC = () => {
+  const unreadChannels = useUnreadStore.use.unreadChannels();
+
+  // For display purposes I don't really care about channel status as It can be set in background
+  // so I'm memoizing it to prevent unnecessary re-renders
+  const data = useMemo(() => unreadChannels, []);
+
+  return <MemoizedListOfChannels unreadChannels={data} />;
 };
