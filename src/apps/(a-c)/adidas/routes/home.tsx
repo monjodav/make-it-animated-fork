32e4f76a54b08015145ebbb94f883f01fe8082b0
Serhii B <s_blyshchak@ukr.net>
2025-09-02 18:14:19 +0300

    fix bug related adidas header

--- src/apps/(a-c)/adidas/routes/home.tsx
+++ src/apps/(a-c)/adidas/routes/home.tsx
@@ -1,17 +1,38 @@
 import { Car, PawPrint, Percent, Search, UserRound } from "lucide-react-native";
 import { View, Text } from "react-native";
-import Animated, { Extrapolation, interpolate, useAnimatedStyle } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Tabs } from "react-native-collapsible-tab-view";
 import { cn } from "@/src/shared/lib/utils/cn";
-import { useTargetMeasurement } from "@/src/shared/lib/hooks/use-target-measurment";
 import { Marquee } from "@/src/shared/components/marquee";
-import { useScrollViewOffset } from "@/src/shared/lib/hooks/use-scroll-view-offset";
 
 // adidas-home-header-marquee-animation 🔽
 
 type MarqueeItemProps = {
   icon: React.ReactNode;
   text: string;
+};
+
+const Header = () => {
+  return (
+    <View className="py-5 border-b border-neutral-200">
+      <Marquee spacing={48} speed={0.6}>
+        <View className="flex-row items-center gap-12">
+          <MarqueeItem
+            icon={<Car size={18} color="black" strokeWidth={1} />}
+            text="Безкоштовна доставка замоовлень від 2500грн"
+          />
+          <MarqueeItem
+            icon={<Percent size={18} color="black" strokeWidth={1} />}
+            text="Знижка 5% при оплаті банківською картою"
+          />
+          <MarqueeItem
+            icon={<PawPrint size={18} color="black" fill="black" strokeWidth={1} />}
+            text="Покупка частинами від monobank"
+          />
+        </View>
+      </Marquee>
+    </View>
+  );
 };
 
 const MarqueeItem = ({ icon, text }: MarqueeItemProps) => {
@@ -39,97 +60,82 @@ const TabItem = ({ label, isActive }: TabItemProps) => {
 export default function Home() {
   const insets = useSafeAreaInsets();
 
-  const { scrollOffsetY, scrollHandler } = useScrollViewOffset();
-
-  const { targetRef, onTargetLayout, measurement } = useTargetMeasurement();
-
-  const rTargetContainerStyle = useAnimatedStyle(() => {
-    if (measurement.value === null) return { transform: [{ translateY: 0 }] };
-
-    const containerHeight = measurement.value.height;
-
-    return {
-      marginBottom: interpolate(
-        scrollOffsetY.value,
-        [0, containerHeight],
-        [0, -containerHeight],
-        Extrapolation.CLAMP
-      ),
-      transform: [
-        {
-          translateY: interpolate(
-            scrollOffsetY.value,
-            [0, containerHeight],
-            [0, -containerHeight],
-            Extrapolation.CLAMP
-          ),
-        },
-      ],
-    };
-  });
-
   return (
-    <View className="flex-1 bg-white" style={{ paddingTop: insets.top }}>
-      {/* Header */}
-      <View className="flex-row items-center justify-between px-4 py-2 border-b border-neutral-200 bg-white z-50">
-        <Text className="text-lg font-semibold uppercase tracking-widest">Вибір Adidas</Text>
-        <View className="flex-row items-center gap-5">
-          <Search size={20} color="black" strokeWidth={1} />
-          <UserRound size={20} color="black" strokeWidth={1} />
-        </View>
-      </View>
-      <Animated.View style={rTargetContainerStyle}>
-        <Animated.View
-          ref={targetRef}
-          className=" px-4 py-3 border-b border-neutral-200"
-          onLayout={onTargetLayout}
-        >
-          <Marquee spacing={48} speed={0.6}>
-            <View className="flex-row items-center gap-12">
-              <MarqueeItem
-                icon={<Car size={18} color="black" strokeWidth={1} />}
-                text="Безкоштовна доставка замоовлень від 2500грн"
-              />
-              <MarqueeItem
-                icon={<Percent size={18} color="black" strokeWidth={1} />}
-                text="Знижка 5% при оплаті банківською картою"
-              />
-              <MarqueeItem
-                icon={<PawPrint size={18} color="black" fill="black" strokeWidth={1} />}
-                text="Покупка частинами від monobank"
-              />
-            </View>
-          </Marquee>
-        </Animated.View>
-        <View className="flex-row gap-10 px-4 py-4 border-b border-neutral-200">
-          <TabItem label="Чоловіки" isActive />
-          <TabItem label="Жінки" />
-          <TabItem label="Діти" />
-          <TabItem label="Новинки" />
-          <TabItem label="Види спорту" />
-        </View>
-      </Animated.View>
-      {/* List  */}
-      <Animated.ScrollView
-        onScroll={scrollHandler}
-        scrollEventThrottle={16}
-        showsVerticalScrollIndicator={false}
-      >
-        <View className="h-[600px] bg-stone-100 justify-end px-6 pb-3">
-          <View className="h-6 w-[100px] bg-white mb-1" />
-          <View className="h-6 w-[130px] bg-white mb-4" />
-          <View className="h-12 w-full bg-white" />
-        </View>
-        <View className="py-16 px-8">
-          <View className="h-6 w-[110px] bg-stone-100 mb-1" />
-          <View className="h-6 w-[140px] bg-stone-100 mb-4" />
-          <View className="flex-row gap-2">
-            <View className="h-[300px] w-[45%] bg-stone-100" />
-            <View className="h-[300px] w-[45%] bg-stone-100" />
+    <>
+      <View className="bg-white z-50" style={{ paddingTop: insets.top }}>
+        <View className="flex-row items-center justify-between px-4 py-2 border-b border-neutral-200 bg-white z-50">
+          <Text className="text-lg font-semibold uppercase tracking-widest">Вибір Adidas</Text>
+
+          <View className="flex-row items-center gap-5">
+            <Search size={20} color="black" strokeWidth={1} />
+            <UserRound size={20} color="black" strokeWidth={1} />
           </View>
         </View>
-      </Animated.ScrollView>
-    </View>
+      </View>
+      <Tabs.Container
+        renderHeader={Header}
+        renderTabBar={() => {
+          return (
+            <View
+              style={{
+                elevation: 0,
+                shadowOpacity: 0,
+                shadowColor: "transparent",
+              }}
+              className="flex-row gap-10 px-4 py-4 border-b border-neutral-200 bg-white"
+            >
+              <TabItem label="Чоловіки" isActive />
+              <TabItem label="Жінки" />
+              <TabItem label="Діти" />
+              <TabItem label="Новинки" />
+              <TabItem label="Види спорту" />
+            </View>
+          );
+        }}
+        headerContainerStyle={{ elevation: 0, shadowOpacity: 0, shadowColor: "transparent" }}
+      >
+        <Tabs.Tab name="Content">
+          <Tabs.ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ backgroundColor: "white" }}
+          >
+            <View className="h-[600px] bg-stone-100 justify-end px-6 pb-3">
+              <View className="h-6 w-[100px] bg-white mb-1" />
+              <View className="h-6 w-[130px] bg-white mb-4" />
+              <View className="h-12 w-full bg-white" />
+            </View>
+
+            <View className="py-16 px-8">
+              <View className="h-6 w-[110px] bg-stone-100 mb-1" />
+              <View className="h-6 w-[140px] bg-stone-100 mb-4" />
+
+              <View className="flex-row gap-2">
+                <View className="h-[300px] w-[45%] bg-stone-100" />
+                <View className="h-[300px] w-[45%] bg-stone-100" />
+              </View>
+            </View>
+            <View className="py-16 px-8">
+              <View className="h-6 w-[110px] bg-stone-100 mb-1" />
+              <View className="h-6 w-[140px] bg-stone-100 mb-4" />
+
+              <View className="flex-row gap-2">
+                <View className="h-[300px] w-[45%] bg-stone-100" />
+                <View className="h-[300px] w-[45%] bg-stone-100" />
+              </View>
+            </View>
+            <View className="py-16 px-8">
+              <View className="h-6 w-[110px] bg-stone-100 mb-1" />
+              <View className="h-6 w-[140px] bg-stone-100 mb-4" />
+
+              <View className="flex-row gap-2">
+                <View className="h-[300px] w-[45%] bg-stone-100" />
+                <View className="h-[300px] w-[45%] bg-stone-100" />
+              </View>
+            </View>
+          </Tabs.ScrollView>
+        </Tabs.Tab>
+      </Tabs.Container>
+    </>
   );
 }
 
