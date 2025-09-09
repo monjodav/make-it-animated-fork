b920fe2bf320f8b518e587d40414bb17c64b21cd
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-09-09 21:41:58 +0300

    remove react-native-inner-shadow

--- src/shared/components/index-screen/explore-animations-btn.tsx
+++ src/shared/components/index-screen/explore-animations-btn.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react";
-import { Text } from "react-native";
+import { Pressable, Text, StyleSheet } from "react-native";
 import Animated, { FadeInDown, interpolate, useAnimatedStyle } from "react-native-reanimated";
 import { DrawerActions } from "@react-navigation/native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import { useIndexAnimation } from "../../lib/providers/index-animation";
-import { ShadowPressable } from "react-native-inner-shadow";
 import { useWindowDimensions } from "react-native";
+import * as Haptics from "expo-haptics";
 
 export const ExploreAnimationsBtn: FC = () => {
   const { width } = useWindowDimensions();
@@ -25,24 +25,23 @@ export const ExploreAnimationsBtn: FC = () => {
   return (
     <Animated.View className="absolute" style={[{ bottom: insets.bottom + 24 }, rContainerStyle]}>
       <Animated.View entering={FadeInDown.delay(1400)}>
-        <ShadowPressable
-          shadowBlur={6}
-          duration={150}
-          damping={0.8}
-          style={{
-            width: width * 0.8,
-            height: 55,
-            backgroundColor: "#FF4A3D",
-            borderRadius: 18,
-            borderCurve: "continuous",
-            justifyContent: "center",
-            alignItems: "center",
+        <Pressable
+          onPress={() => {
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+            navigation.dispatch(DrawerActions.openDrawer());
           }}
-          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+          className="h-16 bg-brand rounded-3xl border-[3px] border-neutral-300/25 flex items-center justify-center"
+          style={[styles.borderCurve, { width: width * 0.8 }]}
         >
           <Text className="text-neutral-50 text-xl font-poppins-semibold">Explore animations</Text>
-        </ShadowPressable>
+        </Pressable>
       </Animated.View>
     </Animated.View>
   );
 };
+
+const styles = StyleSheet.create({
+  borderCurve: {
+    borderCurve: "continuous",
+  },
+});
