07ecccc8e4e9587664c2c68518b4d18a0dd03b7a
Serhii B <s_blyshchak@ukr.net>
2025-09-22 14:24:16 +0300

    add basics of breath

--- src/apps/(m-o)/opal/components/start-timer-button.tsx
+++ src/apps/(m-o)/opal/components/start-timer-button.tsx
@@ -1,48 +1,109 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  LayoutChangeEvent,
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import Svg, { Circle, RadialGradient, Defs, Stop } from "react-native-svg";
 import { simulatePress } from "@/src/shared/lib/utils/simulate-press";
+import { BlurView } from "expo-blur";
+import {
+  Blur,
+  Canvas,
+  Path,
+  processTransform3d,
+  Skia,
+  usePathValue,
+} from "@shopify/react-native-skia";
+import { useSharedValue, withDelay, withRepeat, withTiming } from "react-native-reanimated";
+import { useEffect, useState } from "react";
+
+const DURATION = 2500;
+const PRIMARY_COLOR = "#039e81ff";
+const SECONDARY_COLOR = "#418140ff";
 
 const StartTimerButton = () => {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const ovalWidth = height * 3.4;
+  const ovalHeight = height * 1.7;
+  const centerY = height / 2 + ovalHeight / 2.2;
+
+  const leftOvalRect = {
+    x: ovalWidth / 13,
+    y: centerY - ovalHeight / 2,
+    width: ovalWidth,
+    height: ovalHeight,
+  };
+  const leftOvalPathBase = Skia.Path.Make().addOval(leftOvalRect);
+  const scaleLeft = useSharedValue(1);
+  const leftOvalPath = usePathValue((path) => {
+    "worklet";
+    path.transform(processTransform3d([{ scale: scaleLeft.value }]));
+  }, leftOvalPathBase);
+
+  const rightOvalRect = {
+    x: width - 1.2 * ovalWidth,
+    y: centerY - ovalHeight / 2,
+    width: ovalWidth,
+    height: ovalHeight,
+  };
+  const rightOvalPathBase = Skia.Path.Make().addOval(rightOvalRect);
+  const scaleRight = useSharedValue(1);
+  const rightOvalPath = usePathValue((path) => {
+    "worklet";
+    path.transform(processTransform3d([{ scale: scaleRight.value }]));
+  }, rightOvalPathBase);
+
+  useEffect(() => {
+    scaleLeft.value = withRepeat(withTiming(1.2, { duration: DURATION }), -1, true);
+    scaleRight.value = withDelay(
+      DURATION / 1.25,
+      withRepeat(withTiming(1.2, { duration: DURATION }), -1, true)
+    );
+  }, []);
+
+  const handleLayout = (event: LayoutChangeEvent) => {
+    const { width, height } = event.nativeEvent.layout;
+    setWidth(width);
+    setHeight(height);
+  };
+
   return (
-    <TouchableOpacity activeOpacity={0.5} onPress={simulatePress}>
-      <LinearGradient
-        colors={["#039278ff", "#3d953cff"]}
-        start={{ x: 0.3, y: 0 }}
-        end={{ x: 0.7, y: 0 }}
-        style={styles.gradient}
-      >
-        <View className="flex justify-center items-center absolute top-0 left-0 right-0 bottom-0">
-          <Svg height="900" width="900">
-            <Defs>
-              <RadialGradient id="grad" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-                <Stop offset="50%" stopColor="#ffffff" stopOpacity="0" />
-                <Stop offset="90%" stopColor="#000000" stopOpacity="0.9" />
-              </RadialGradient>
-            </Defs>
-            <Circle cx="450" cy="822" r="450" fill="url(#grad)" />
-          </Svg>
-        </View>
-        <View className="absolute top-0 left-0 right-0 bottom-0 flex-row gap-1.5 items-center justify-center">
-          <Ionicons name="play" size={20} color="white" />
-          <Text className="text-white text-xl font-medium">Start Timer</Text>
-        </View>
-      </LinearGradient>
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={simulatePress}
+      onLayout={handleLayout}
+      className="border border-[#333333] h-[55px] rounded-full mx-3 mb-4 overflow-hidden"
+    >
+      {width > 0 && height > 0 && (
+        <>
+          {Platform.OS === "ios" && (
+            <BlurView intensity={50} tint="dark" style={StyleSheet.absoluteFillObject} />
+          )}
+          <Canvas
+            style={{
+              flex: 1,
+            }}
+          >
+            <Path path={leftOvalPath} color={PRIMARY_COLOR}>
+              <Blur blur={30} />
+            </Path>
+            <Path path={rightOvalPath} color={SECONDARY_COLOR}>
+              <Blur blur={30} />
+            </Path>
+          </Canvas>
+          <View className="absolute top-0 left-0 right-0 bottom-0 flex-row gap-1.5 items-center justify-center">
+            <Ionicons name="play" size={20} color="white" />
+            <Text className="text-white text-xl font-medium">Start Timer</Text>
+          </View>
+        </>
+      )}
     </TouchableOpacity>
   );
 };
 
 export default StartTimerButton;
-
-const styles = StyleSheet.create({
-  gradient: {
-    borderWidth: 1,
-    borderColor: "#333333",
-    height: 55,
-    borderRadius: 50,
-    marginHorizontal: 12,
-    marginBottom: 15,
-    overflow: "hidden",
-  },
-});
