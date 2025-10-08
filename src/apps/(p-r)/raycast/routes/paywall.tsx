cc8aaef777989755ed76bf04206c0e2438d4050b
Serhii B <s_blyshchak@ukr.net>
2025-10-08 11:41:54 +0300

    implement basics

---
+++ src/apps/(p-r)/raycast/routes/paywall.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { View, Text } from "react-native";
+
+export const Paywall: FC = () => {
+  return (
+    <View>
+      <Text>This is the paywall screen</Text>
+    </View>
+  );
+};
