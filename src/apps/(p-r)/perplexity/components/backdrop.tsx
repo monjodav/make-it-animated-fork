1133ea8536a2bb9f21e8c58e4a1f6a4cbdf24937
Serhii B <s_blyshchak@ukr.net>
2025-10-15 12:21:23 +0300

    refactor backdrop

--- src/apps/(p-r)/perplexity/components/backdrop.tsx
+++ src/apps/(p-r)/perplexity/components/backdrop.tsx
@@ -1,66 +1,40 @@
 import { FC } from "react";
 import { BottomSheetBackdrop, BottomSheetBackdropProps } from "@gorhom/bottom-sheet";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { View, StyleSheet, useColorScheme } from "react-native";
-import Animated, {
-  interpolate,
-  useAnimatedStyle,
-  Extrapolation,
-  useDerivedValue,
-} from "react-native-reanimated";
+import { StyleSheet } from "react-native";
+import Animated, { interpolate, Extrapolation, useAnimatedProps } from "react-native-reanimated";
 import { BlurView } from "expo-blur";
 
 const AnimatedBlurView = Animated.createAnimatedComponent(BlurView);
 
-type Props = BottomSheetBackdropProps;
-
-export const Backdrop: FC<Props> = ({ animatedIndex, style }) => {
-  const insets = useSafeAreaInsets();
-
-  // const containerStyle = useAnimatedStyle(() => {
-  //   return {
-  //     opacity: interpolate(animatedIndex.value, [-1, 0, 1], [0, 0, 1], Extrapolation.CLAMP),
-  //   };
-  // });
-
-  // useDerivedValue(() => {
-  //   console.log(animatedIndex);
-  // });
-
-  const blurIntensity = useDerivedValue(() => {
+export const Backdrop: FC<BottomSheetBackdropProps> = ({ animatedIndex, style, ...props }) => {
+  const animatedIntensity = useAnimatedProps(() => {
     const maxIntensity = 25;
-
-    return interpolate(
+    const intensity = interpolate(
       animatedIndex.value,
-      [-1, -0.5, 0, 1, 2], // support potential extra snap >1
+      [-1, -0.5, 0, 1, 2],
       [0, 0, maxIntensity / 2, maxIntensity, 0],
       Extrapolation.CLAMP
     );
-  }, []);
 
-  const animatedIntensity = useDerivedValue(
-    () => blurIntensity.value as number | undefined,
-    [blurIntensity]
-  );
+    return {
+      intensity,
+    };
+  });
 
   return (
-    // <BottomSheetBackdrop
-    //   animatedIndex={animatedIndex}
-    //   disappearsOnIndex={-1}
-    //   appearsOnIndex={0}
-    //   opacity={1}
-    //   {...props}
-    // >
-    <Animated.View
-      pointerEvents={animatedIndex.value < 0 ? "none" : "auto"}
-      style={[StyleSheet.absoluteFill, style]}
+    <BottomSheetBackdrop
+      animatedIndex={animatedIndex}
+      disappearsOnIndex={-1}
+      appearsOnIndex={0}
+      opacity={1}
+      style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+      {...props}
     >
-      <AnimatedBlurView intensity={animatedIntensity} tint="dark" style={StyleSheet.absoluteFill} />
-
-      {/* <View style={[StyleSheet.absoluteFill, { backgroundColor: "rgba(0,0,0,0.40)" }]} /> */}
-
-      <View pointerEvents="none" style={{ paddingTop: insets.top }} />
-    </Animated.View>
-    // </BottomSheetBackdrop>
+      <AnimatedBlurView
+        animatedProps={animatedIntensity}
+        tint="dark"
+        style={StyleSheet.absoluteFill}
+      />
+    </BottomSheetBackdrop>
   );
 };
