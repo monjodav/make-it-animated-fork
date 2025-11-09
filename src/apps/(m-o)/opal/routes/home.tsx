f28779d801b4f92b8643d0af9747b59ebba2ba04
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-11-09 13:09:28 +0200

    add time-slider folder

--- src/apps/(m-o)/opal/routes/home.tsx
+++ src/apps/(m-o)/opal/routes/home.tsx
@@ -1,13 +1,38 @@
-import { FC } from "react";
-import { View } from "react-native";
+import { FC, useState } from "react";
+import { Text, useWindowDimensions, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import StartTimerButton from "../components/start-timer-button";
+import TimeSlider from "../components/time-slider";
+
+const HEIGHT = 44;
 
 export const Home: FC = () => {
+  const [sliderValue, setSliderValue] = useState(5);
+
   const insets = useSafeAreaInsets();
+  const { width } = useWindowDimensions();
+
+  const formatMinutes = (m: number) => {
+    const hours = Math.floor(m / 60);
+    const minutes = m % 60;
+    if (hours === 0) return `${minutes}m`;
+    if (minutes === 0) return `${hours}h`;
+    return `${hours}h ${minutes}m`;
+  };
 
   return (
     <View className="flex-1 pb-2 bg-black justify-end" style={{ paddingTop: insets.top }}>
+      <View className="flex-1 items-center justify-center gap-10">
+        <Text className="text-xl">{formatMinutes(sliderValue)}</Text>
+        <TimeSlider
+          sliderWidth={width * 0.7}
+          sliderHeight={HEIGHT}
+          dividerCount={35}
+          min={5}
+          max={180}
+          onValueChange={(value) => setSliderValue(value)}
+        />
+      </View>
       {/* opal-start-timer-button-animation 🔽 */}
       <StartTimerButton />
       {/* opal-start-timer-button-animation 🔼 */}
