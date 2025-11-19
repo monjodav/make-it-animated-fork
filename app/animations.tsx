3babd3aa3f4dce11beffb7d62e495e7148082da9
Serhii B <s_blyshchak@ukr.net>
2025-11-19 15:56:04 +0200

    add switcher

--- app/animations.tsx
+++ app/animations.tsx
@@ -3,6 +3,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Results } from "@/src/shared/components/animations-screen/results";
 import { AlgoliaProvider } from "@/src/shared/lib/providers/algolia-provider";
 import SearchInput from "@/src/shared/components/animations-screen/search-input";
+import Switcher from "@/src/shared/components/animations-screen/switcher";
 
 export default function Animations() {
   const { top } = useSafeAreaInsets();
@@ -11,6 +12,7 @@ export default function Animations() {
     <AlgoliaProvider>
       <View className="flex-1 bg-black px-4" style={{ paddingTop: top + 12 }}>
         <SearchInput />
+        <Switcher />
         <Results />
       </View>
     </AlgoliaProvider>
---
+++ src/shared/components/animations-screen/switcher.tsx
@@ -0,0 +1,19 @@
+import { Pressable, View } from "react-native";
+import { FC } from "react";
+import { LayoutGrid, List } from "lucide-react-native";
+import { simulatePress } from "../../lib/utils/simulate-press";
+
+const Switcher: FC = () => {
+  return (
+    <View className="flex-row items-center gap-5 self-end my-4">
+      <Pressable onPress={simulatePress}>
+        <List size={25} strokeWidth={3} color="#a8a29e" />
+      </Pressable>
+      <Pressable onPress={simulatePress}>
+        <LayoutGrid size={25} color="#a8a29e" />
+      </Pressable>
+    </View>
+  );
+};
+
+export default Switcher;
