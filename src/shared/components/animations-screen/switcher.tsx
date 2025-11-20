e65adc070bd623912cb30dc7bc22c1a038c80678
Serhii B <s_blyshchak@ukr.net>
2025-11-20 16:54:47 +0200

    implement base slice

--- src/shared/components/animations-screen/switcher.tsx
+++ src/shared/components/animations-screen/switcher.tsx
@@ -1,16 +1,19 @@
 import { Pressable, View } from "react-native";
 import { FC } from "react";
 import { LayoutGrid, List } from "lucide-react-native";
-import { simulatePress } from "../../lib/utils/simulate-press";
+import { useAnimationsStore } from "../../lib/store/animations";
 
 const Switcher: FC = () => {
+  const viewMode = useAnimationsStore((state) => state.viewMode);
+  const setViewMode = useAnimationsStore((state) => state.setViewMode);
+
   return (
     <View className="flex-row items-center gap-5 self-end mt-1">
-      <Pressable onPress={simulatePress}>
-        <List size={25} strokeWidth={3} color="#a8a29e" />
+      <Pressable onPress={() => setViewMode("list")}>
+        <List size={25} strokeWidth={3} color={viewMode === "list" ? "#ffffff" : "#a8a29e"} />
       </Pressable>
-      <Pressable onPress={simulatePress}>
-        <LayoutGrid size={25} color="#a8a29e" />
+      <Pressable onPress={() => setViewMode("grid")}>
+        <LayoutGrid size={25} color={viewMode === "grid" ? "#ffffff" : "#a8a29e"} />
       </Pressable>
     </View>
   );
--- src/shared/lib/store/animations/index.ts
+++ src/shared/lib/store/animations/index.ts
@@ -1,13 +1,16 @@
 import { create } from "zustand";
 import { SearchSlice, createSearchSlice } from "./search";
 import { FiltersSlice, createFiltersSlice } from "./filters";
+import { BaseSlice, createBaseSlice } from "./base";
 
-type AnimationsStore = SearchSlice & FiltersSlice;
+type AnimationsStore = SearchSlice & FiltersSlice & BaseSlice;
 
 export const useAnimationsStore = create<AnimationsStore>()((...a) => ({
   ...createSearchSlice(...a),
   ...createFiltersSlice(...a),
+  ...createBaseSlice(...a),
 }));
 
 export type { FilterType, SelectedFilters } from "./filters";
+export type { ViewMode } from "./base";
 export { filterData } from "./filters";
