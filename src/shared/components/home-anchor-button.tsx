57be508e534be6b91054e53f9d075b33bdd66bad
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-11-23 18:36:46 +0200

    update HomeAnchorButton layout

--- src/shared/components/home-anchor-button.tsx
+++ src/shared/components/home-anchor-button.tsx
@@ -1,41 +1,40 @@
-import { useNavigation, useRouter } from "expo-router";
-import { AlignLeft } from "lucide-react-native";
+import { useNavigation } from "expo-router";
+import { Home } from "lucide-react-native";
 import React, { FC } from "react";
-import { Pressable, StyleSheet } from "react-native";
-import Animated, { useSharedValue } from "react-native-reanimated";
-import * as Haptics from "expo-haptics";
-import { KeyboardController } from "react-native-keyboard-controller";
+import { Pressable, StyleSheet, View } from "react-native";
+import Animated from "react-native-reanimated";
 import { fireHaptic } from "../lib/utils/fire-haptic";
 import { useAppStore } from "../lib/store/app";
+
+const SIZE = 44;
 
 export const HomeAnchorButton: FC = () => {
   const navigation = useNavigation();
   const setIsHomeAnchorButtonPressed = useAppStore.use.setIsHomeAnchorButtonPressed();
 
-  const opacity = useSharedValue(0);
-
   return (
-    <Animated.View
-      className="absolute top-0 bottom-0 left-0 justify-center pl-3 pointer-events-box-none z-[9999]"
-      // style={{ opacity }}
-    >
+    <Animated.View className="absolute inset-0 pointer-events-box-none z-[9999]">
       <Pressable
-        className="size-10 items-center justify-center rounded-xl bg-brand"
-        style={styles.borderCurve}
+        className="absolute left-[85%] top-[50%] items-center justify-center rounded-full bg-brand"
+        style={styles.container}
         onPress={() => {
           fireHaptic();
           setIsHomeAnchorButtonPressed(true);
           navigation.goBack();
         }}
       >
-        <AlignLeft size={20} color="white" />
+        <View className="mb-0.5">
+          <Home size={20} color="white" fill="white" />
+        </View>
       </Pressable>
     </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
-  borderCurve: {
+  container: {
+    width: SIZE,
+    height: SIZE,
     borderCurve: "continuous",
   },
 });
