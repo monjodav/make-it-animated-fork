cf3b6ec566c626a7a2971544041673785c9e24b4
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-12-02 20:48:04 +0200

    feat: add UpdateComingMessage component

--- app/[...unmatched].tsx
+++ app/[...unmatched].tsx
@@ -1,16 +1,13 @@
-import { MANUAL_ERROR_CAPTURE } from "@/src/shared/lib/utils/sentry";
-import { Redirect, usePathname } from "expo-router";
+import { Redirect } from "expo-router";
 import React, { FC, useEffect } from "react";
+import { useAppStore } from "@/src/shared/lib/store/app";
 
 const Screen: FC = () => {
-  const pathname = usePathname();
+  const setShowUpdateComingMessage = useAppStore.use.setShowUpdateComingMessage();
 
   useEffect(() => {
-    MANUAL_ERROR_CAPTURE({
-      title: "Unmatched route > Pathname details",
-      error: new Error(`Unmatched route: ${pathname}`),
-    });
-  }, [pathname]);
+    setShowUpdateComingMessage(true);
+  }, [setShowUpdateComingMessage]);
 
   return <Redirect href="/" />;
 };
