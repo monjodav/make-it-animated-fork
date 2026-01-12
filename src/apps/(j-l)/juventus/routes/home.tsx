63eae2ee41a8dcbe0a8c2b33b996073dde3cc82b
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2026-01-12 22:47:20 +0200

    chore(juventus): update android msg

--- src/apps/(j-l)/juventus/routes/home.tsx
+++ src/apps/(j-l)/juventus/routes/home.tsx
@@ -6,7 +6,9 @@ import { useAndroidNote } from "@/src/shared/lib/hooks/use-android-note";
 // juventus-games-calendar-animation 🔽
 
 const Home = () => {
-  useAndroidNote("");
+  useAndroidNote(
+    "Scroll-based animations on Android devices may appear jumpy. This is a known issue in React Native that many developers are actively working to resolve. We are monitoring related issue tickets and will continue to improve the implementation as solutions become available."
+  );
 
   return (
     <View className="flex-1 bg-black">
